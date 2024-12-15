0784c51d5824debb11fe617f3cba7c8394d5d2f4
fitse01 <fitsumtafese01@gmail.com>
2024-12-15 21:32:18 +0300

    Tailwind CSS Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center text-3xl font-bold text-white">
+      <h1 className="text-center text-3xl font-bold text-secondary">
         Wellcome to the world of Next js{" "}
       </h1>
     </div>
