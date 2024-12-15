ce52fa861ea6f11e123cb035027ebde84c64bb3c
fitse01 <fitsumtafese01@gmail.com>
2024-12-15 16:34:28 +0300

    ESlint & Prettier Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,51 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals", // Next.js recommended rules
+    "next/typescript", // TypeScript rules for Next.js
+    "standard", // Add the eslint-config-standard rules
+    "plugin:tailwindcss/recommended", // Tailwind CSS plugin rules
+    "prettier" // Prettier integration
+  ),
+  {
+    plugins: ["import"],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // Built-in types are first
+            "external", // External libraries
+            "internal", // Internal modules
+            ["parent", "sibling"], // Parent and sibling types can be mingled together
+            "index", // Then the index file
+            "object", // Object imports
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    ignorePatterns: ["components/ui/**"],
+  },
+  {
+    files: ["*.ts", "*.tsx"],
+    rules: {
+      "no-undef": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
