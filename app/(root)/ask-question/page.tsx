d5b65f2a4bd9e28dd5afe3d17d8fd2b88c68a835
fitse01 <fitsumtafese01@gmail.com>
2025-07-10 01:45:34 +0300

    Mobile Nav and NavLinks setup

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AskAquestion = () => {
+  return <div>AskAquestion</div>;
+};
+
+export default AskAquestion;
--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Theme from "./Theme";
+import MobileNavigation from "./MobileNavigation";
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300  dark:shadow-none sm:px-12 ">
@@ -19,6 +20,7 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );
