f17032dca6561d3ec1b5b580d9f401db66ae936a
fitse01 <fitsumtafese01@gmail.com>
2025-08-31 23:48:47 +0300

    Home Layout initiated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,30 @@
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 import React, { use } from "react";
 
 const Home = async () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Welcome to DevFlow</h1>
-      <p className="mt-4 text-lg">
-        This is the home page of your application. You can start building your
-        features here.
-      </p>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Question</h1>
+        <Button
+          className="primary-gradient min-h-[48px] px-4 py-3 !text-light-900 "
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+
+      <section className="mt-11">Local Search </section>
+      <section className="mt-5">HomeFilter</section>
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+      </div>
+    </>
   );
 };
 
