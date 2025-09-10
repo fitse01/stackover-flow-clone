ce2394a08e9084c1bd50b11aa3732cec6f762a6f
fitse01 <fitsumtafese01@gmail.com>
2025-09-10 03:00:10 +0300

    MDXEditor implemented

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,6 +1,5 @@
 "use client";
-// InitializedMDXEditor.tsx
-import { use, type ForwardedRef } from "react";
+
 import {
   headingsPlugin,
   listsPlugin,
@@ -9,10 +8,32 @@ import {
   markdownShortcutPlugin,
   MDXEditor,
   type MDXEditorMethods,
+  ConditionalContents,
+  ChangeCodeMirrorLanguage,
+  toolbarPlugin,
+  UndoRedo,
+  Separator,
+  BoldItalicUnderlineToggles,
+  ListsToggle,
+  CreateLink,
+  InsertImage,
+  InsertTable,
+  InsertThematicBreak,
+  InsertCodeBlock,
+  linkPlugin,
+  linkDialogPlugin,
+  tablePlugin,
+  imagePlugin,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  diffSourcePlugin,
 } from "@mdxeditor/editor";
 import { basicDark } from "cm6-theme-basic-dark";
-import "./dark-editor.css";
 import { useTheme } from "next-themes";
+import type { ForwardedRef } from "react";
+
+import "@mdxeditor/editor/style.css";
+import "./dark-editor.css";
 
 interface Props {
   value: string;
@@ -20,26 +41,86 @@ interface Props {
   editorRef: ForwardedRef<MDXEditorMethods> | null;
 }
 
-// Only import this to the next file
-
 const Editor = ({ value, editorRef, fieldChange, ...props }: Props) => {
   const { resolvedTheme } = useTheme();
-  const theme = resolvedTheme === "dark" ? basicDark : undefined;
+
+  const theme = resolvedTheme === "dark" ? [basicDark] : [];
+
   return (
     <MDXEditor
+      key={resolvedTheme}
       markdown={value}
+      ref={editorRef}
       className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border"
       onChange={fieldChange}
       plugins={[
-        // Example Plugin Usage
         headingsPlugin(),
         listsPlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
         quotePlugin(),
         thematicBreakPlugin(),
         markdownShortcutPlugin(),
+        tablePlugin(),
+        imagePlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            css: "css",
+            txt: "txt",
+            sql: "sql",
+            html: "html",
+            saas: "saas",
+            scss: "scss",
+            bash: "bash",
+            json: "json",
+            js: "javascript",
+            ts: "typescript",
+            "": "unspecified",
+            tsx: "TypeScript (React)",
+            jsx: "JavaScript (React)",
+          },
+          autoLoadLanguageSupport: true,
+          codeMirrorExtensions: theme,
+        }),
+        diffSourcePlugin({ viewMode: "rich-text", diffMarkdown: "" }),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <ConditionalContents
+              options={[
+                {
+                  when: (editor) => editor?.editorType === "codeblock",
+                  contents: () => <ChangeCodeMirrorLanguage />,
+                },
+                {
+                  fallback: () => (
+                    <>
+                      <UndoRedo />
+                      <Separator />
+
+                      <BoldItalicUnderlineToggles />
+                      <Separator />
+
+                      <ListsToggle />
+                      <Separator />
+
+                      <CreateLink />
+                      <InsertImage />
+                      <Separator />
+
+                      <InsertTable />
+                      <InsertThematicBreak />
+
+                      <InsertCodeBlock />
+                    </>
+                  ),
+                },
+              ]}
+            />
+          ),
+        }),
       ]}
       {...props}
-      ref={editorRef}
     />
   );
 };
