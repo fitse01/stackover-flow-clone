a66ebf5492e369b602232e847c99055b3283ea9e
fitse01 <fitsumtafese01@gmail.com>
2025-09-10 03:49:59 +0300

    implement question form Tag Input

--- components/navigation/LeftSidebar.tsx
+++ components/navigation/LeftSidebar.tsx
@@ -59,12 +59,12 @@ import Image from "next/image";
 
 const LeftSidebar = () => {
   return (
-    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-full flex flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-full flex flex-col justify-between overflow-y-auto border-r p-6 pt-28 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
         <NavLinks />
       </div>
 
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3 mt-5">
         <Button
           className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
           asChild
