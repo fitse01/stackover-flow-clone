9036cf90264a13c6444d08e8691d0c06df5bab44
fitse01 <fitsumtafese01@gmail.com>
2025-10-03 22:41:24 +0300

    implement CRUD operation for ticketing example

---
+++ app/database.ts
@@ -0,0 +1,16 @@
+const tickets = [
+  {
+    id: 1,
+    name: "Fix Next.js API route bug",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 2,
+    name: "Next.js API route bug",
+    status: "closed",
+    type: "bug",
+  },
+];
+
+export default tickets;
